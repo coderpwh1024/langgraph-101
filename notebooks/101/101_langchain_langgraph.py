54f02ae6bf92ba2ec05ce28d9f75442d6f9e7abf
pengwenhao <pengwenhao@aliyun.com>
2026-05-20 15:44:31 +0800

    feat:init

--- notebooks/101/101_langchain_langgraph.py
+++ notebooks/101/101_langchain_langgraph.py
@@ -1,1 +1,11 @@
 
+import sys
+from pathlib import Path
+
+
+project_root = Path().resolve().parser.parent
+
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+
+from utils.models import model
