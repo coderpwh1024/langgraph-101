bb13a5224438a0bc0e33b237a068a6d91d88fca9
pengwenhao <pengwenhao@aliyun.com>
2026-06-09 10:38:13 +0800

    feat:init multi_agent.py

---
+++ notebooks/201/multi_agent.py
@@ -0,0 +1,9 @@
+import  sys
+from pathlib import Path
+
+
+project_root =Path().resolve().parent.parent
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+
+from utils.models import model
